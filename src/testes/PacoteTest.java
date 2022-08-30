c2bb75ad27e8ad34af7ad9dbfd3d068afd49bc4a
Tobias Dornelles <tobiasdornelles@hotmail.com>
2022-08-30 18:17:18 -0300

    Initial commit

---
+++ src/testes/PacoteTest.java
@@ -0,0 +1,73 @@
+package testes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+import pacotes.Pacote;
+
+
+class PacoteTest {
+
+    double resultadoEsperado, resultadoProduzido;
+    Pacote p;
+
+    @Test
+    void testPositivo() {
+        //massa de dados de valores positivos
+        HashMap<Pacote, Double> massa = new HashMap<>();
+
+        //preencher massa de dados
+        massa.put(new Pacote(' ','A',' ',10), 30.0);
+        massa.put(new Pacote(' ','A',' ',20), 60.0);
+        massa.put(new Pacote('L','S','E',10), 20.0);
+        massa.put(new Pacote('L','S','E',19.9), 39.8);
+        massa.put(new Pacote('L','S','E',20), 40.0);
+        massa.put(new Pacote('L','S','E',20.1), 40.2);
+
+        //processamento com foreach
+        massa.forEach((pacote, freteEsperado) ->{
+            assertEquals(freteEsperado, pacote.calcularFrete());});
+    }
+
+    @Test
+    void testNegativo() {
+    //cod erro tipo de entrega esta errado na classe
+
+        //massa de dados de valores negativos
+        HashMap<Pacote, Double> massa = new HashMap<>();
+
+        massa.put(new Pacote('L','S','H',10), -4d);
+        massa.put(new Pacote(' ','K',' ',20), -3d);
+        massa.put(new Pacote('Y','S','E',10), -2d);
+        massa.put(new Pacote('F','A','N','a'), -1d);
+        massa.put(new Pacote('L','S','E',-10), -1d);
+        massa.put(new Pacote('L','S','E',20), 40.0);
+        massa.put(new Pacote('L','S','E',20.8), 41.6);
+
+        //processamento com foreach
+        massa.forEach((pacote, freteEsperado) ->{
+            assertEquals(freteEsperado, pacote.calcularFrete());});
+    }
+
+    @Test
+    void testLimites() {
+        //massa de dados de valores positivos
+        HashMap<Pacote, Double> massa = new HashMap<>();
+
+        //preencher massa de dados
+        massa.put(new Pacote(' ','A',' ',1.99), 11.94);
+        massa.put(new Pacote(' ','A',' ',2.01), 6.03);
+        massa.put(new Pacote(' ','A',' ',19.99), 59.97);
+        massa.put(new Pacote(' ','A',' ',20.01), 40.02);
+        massa.put(new Pacote('L','S','E',10), 20.0);
+        massa.put(new Pacote('L','S','E',19.99), 39.98);
+        massa.put(new Pacote('L','S','E',20.01), 40.02);
+
+        //processamento com foreach
+        massa.forEach((pacote, freteEsperado) ->{
+            assertEquals(freteEsperado, pacote.calcularFrete());});
+    }
+
+}
